9aba9aba4ffda03f5574b1a666448d3c46fb5da7
gmarseglia <g.marseglia.it@gmail.com>
2024-04-22 19:06:53 +0200

    close #9

--- src/main/java/it/gmarseglia/app/controller/DatasetController.java
+++ src/main/java/it/gmarseglia/app/controller/DatasetController.java
@@ -3,7 +3,6 @@ package it.gmarseglia.app.controller;
 
 import it.gmarseglia.app.boundary.CsvEntryBoundary;
 import it.gmarseglia.app.model.Entry;
-import it.gmarseglia.app.model.Version;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.util.*;
@@ -12,16 +11,12 @@ public class DatasetController {
 
     private static final Map<String, DatasetController> instances = new HashMap<>();
 
-    private final VersionsController vc;
-    private final GitController gc;
     private final EntriesController ec;
     private final CsvEntryBoundary cb;
     private final MyLogger logger = MyLogger.getInstance(this.getClass());
 
 
     private DatasetController(String projName) {
-        this.vc = VersionsController.getInstance(projName);
-        this.gc = GitController.getInstance(projName);
         this.ec = EntriesController.getInstance(projName);
         this.cb = new CsvEntryBoundary(projName);
     }
@@ -33,14 +28,8 @@ public class DatasetController {
 
     /**
      * Populate the dataset. It:
-     * 1. takes all the versions from Jira.
-     * 2. tries to check out each version using GitHub tickets.
-     * If the version is not present, then it's not considered "valid".
-     * 3. gets the last commit date and assigns it to the version.
-     * Here the version becomes <code>Version.java</code> from <code>JiraVersion.java</code>.
-     * 4. discards the latest half versions.
-     * 5. finds all '*src*.java' files in the repo, after check out to each valid version.
-     * 5. writes those entries to the .csv file.
+     * 1. Gets the entries for half the versions.
+     * 2. Writes those entries to the .csv file.
      *
      * @param verbose option
      * @throws GitAPIException uses <code>GitController</code>
@@ -48,42 +37,11 @@ public class DatasetController {
     public void populateDataset(boolean verbose) throws GitAPIException {
         MyLogger.setStaticVerbose(verbose);
 
-        // Get the oldest half valid versions
-        List<Version> halfVersions = this.vc.getHalfVersion();
+        List<Entry> allDatasetEntries = ec.getAllEntriesForHalfVersions();
 
-        logger.log(() -> System.out.printf("Half valid version count: %d\n", halfVersions.size()));
-
-        // for each version, get all the .java src files
-        for (Version v : halfVersions) {
-            appendEntriesForVersion(v);
-            logger.log(() -> System.out.printf("Total count: %d\n", ec.getAllEntries().size()));
-        }
+        logger.log(() -> System.out.printf("Total entries size: %d\n", allDatasetEntries.size()));
 
         // write all the found entries on the .csv files
-        cb.writeEntries(ec.getAllEntries());
-    }
-
-    /**
-     * 1. Checks out to a specific version.
-     * 2. Gets all the "*src*.java" files.
-     * 3. Appends them using {@link EntriesController}.{@code findAndAppendEntries}
-     *
-     * @param version Version to check out.
-     */
-    private void appendEntriesForVersion(Version version) {
-        try {
-            logger.log(() -> System.out.printf("Release %s:", version.getName()));
-
-            // checkout local Git dir at given version
-            gc.checkoutByTag(version.getName());
-
-            // get all the .jav src files in given version
-            List<Entry> entriesByVersion = ec.findAndAppendEntries(gc.getLocalPath(), version);
-
-            logger.log(() -> System.out.printf(" %d .java src files found.\n", entriesByVersion.size()));
-
-        } catch (GitAPIException e) {
-            logger.log(() -> System.out.print("\tnot found on Git.\n"));
-        }
+        cb.writeEntries(allDatasetEntries);
     }
 }
--- src/main/java/it/gmarseglia/app/controller/EntriesController.java
+++ src/main/java/it/gmarseglia/app/controller/EntriesController.java
@@ -2,6 +2,7 @@ package it.gmarseglia.app.controller;
 
 import it.gmarseglia.app.model.Entry;
 import it.gmarseglia.app.model.Version;
+import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -14,35 +15,62 @@ public class EntriesController {
     private static final Map<String, EntriesController> instances = new HashMap<>();
 
     private final List<Entry> allEntries = new ArrayList<>();
+    private final MyLogger logger = MyLogger.getInstance(this.getClass());
+    private final GitController gc;
+    private final VersionsController vc;
 
-    public static EntriesController getInstance(String projName){
-        instances.computeIfAbsent(projName, string -> new EntriesController());
+    private EntriesController(String projName) {
+        this.gc = GitController.getInstance(projName);
+        this.vc = VersionsController.getInstance(projName);
+    }
+
+    public static EntriesController getInstance(String projName) {
+        instances.computeIfAbsent(projName, string -> new EntriesController(projName));
         return instances.get(projName);
     }
 
     /**
-     * @param localPath Path of the dir in which the files are expected
-     * @param version   Version to label the entries.
+     * 1. Checks out to a specific version.
+     * 2. Gets all the "*src*.java" files.
+     *
+     * @param version Version to label the entries.
      * @return List of entries (filename, version) for given version.
      */
-    public List<Entry> findAndAppendEntries(Path localPath, Version version) {
-        List<Path> javaSrcPathsForVersion = MyFileUtils.getAllJavaSrcFiles(localPath);
+    public List<Entry> findAndAppendEntries(Version version) {
+        List<Entry> result = new ArrayList<>();
 
-        List<Entry> entriesForVersion = new ArrayList<>();
+        try {
+            logger.log(() -> System.out.printf("Release %s: ", version.getName()));
 
-        for (Path p : javaSrcPathsForVersion) {
-            entriesForVersion.add(new Entry(p, version, p.toString().replace(localPath.toString(), "")));
+            // checkout local Git dir at given version
+            gc.checkoutByTag(version.getName());
+
+            // get all the .java src files in given version
+            Path localPath = this.gc.getLocalPath();
+            MyFileUtils.getAllJavaSrcFiles(localPath)
+                    .forEach(p -> result.add(new Entry(p, version, p.toString().replace(localPath.toString(), ""))));
+
+            logger.log(() -> System.out.printf("%d .java src files found.\n", result.size()));
+
+        } catch (GitAPIException e) {
+            logger.log(() -> System.out.print("not found on Git.\n"));
         }
 
-        this.allEntries.addAll(entriesForVersion);
-
-        return entriesForVersion;
+        return result;
     }
 
     /**
-     * @return The entries for every version appended since creation.
+     * 1. Gets half the versions.
+     * 2. Appends entries for each every version
+     *
+     * @return The entries for every version.
      */
-    public List<Entry> getAllEntries() {
-        return this.allEntries;
+    public List<Entry> getAllEntriesForHalfVersions() throws GitAPIException {
+        List<Entry> result = new ArrayList<>();
+
+        vc.getHalfVersion()
+                .forEach(version -> result.addAll(this.findAndAppendEntries(version)));
+
+        return result;
     }
 }
