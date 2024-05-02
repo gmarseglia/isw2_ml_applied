3e05f3a8e03327d3b38c90275c08b8a894c38e7d
gmarseglia <g.marseglia.it@gmail.com>
2024-05-02 15:04:02 +0200

    #20

--- src/main/java/it/gmarseglia/app/controller/BugginessController.java
+++ src/main/java/it/gmarseglia/app/controller/BugginessController.java
@@ -27,44 +27,82 @@ public class BugginessController {
     public List<Entry> getAllLabelledEntries() throws GitAPIException {
         if (this.allLabelledEntries == null) {
             EntriesController ec = EntriesController.getInstance(projName);
-            this.allLabelledEntries = new ArrayList<>(ec.getAllEntriesForHalfVersions());
-
-            ProportionController pc = ProportionController.getInstance(projName);
-            List<Issue> allValidProportionedIssues = pc.getTotalProportionedIssues(Integer.MAX_VALUE);
-
             VersionsController vc = VersionsController.getInstance(projName);
             BlameController bc = BlameController.getInstance(projName);
 
+            // #1. Obtain the list of all entries for half the usable versions
+            this.allLabelledEntries = new ArrayList<>(ec.getAllEntriesForHalfVersions());
+
+            // #2. Obtain the list of proportioned issues
+            ProportionController pc = ProportionController.getInstance(projName);
+            List<Issue> allValidProportionedIssues = pc.getTotalProportionedIssues(Integer.MAX_VALUE);
+
+            // Obtain the index of the last half usable versions
             int HALF_LASTi = vc.getAllReleasedVersions().indexOf(vc.getHalfVersion().getLast());
 
-            int valid = 0;
-            int invalid = 0;
+            int usable = 0;
+            int unusable = 0;
             int overHalf = 0;
 
+            // For print purpose
             List<Entry> newLabelledEntries = new ArrayList<>();
 
             for (Issue proportionedIssue : allValidProportionedIssues) {
+
+                // Actual IV = "Jira IV" if present, otherwise "Predicted IV"
                 int IVi = proportionedIssue.IVIndex() != null ? proportionedIssue.IVIndex() : proportionedIssue.PredictedIVIndex();
+
+                // Actual FV = "Jira FV" if in valid versions, otherwise "Practically last possible FV"
                 int FVi = proportionedIssue.FVIndex() != null ? proportionedIssue.FVIndex() : HALF_LASTi + 1;
 
-                // skip predicted nonPostRelease issues
+                // skip nonPostRelease && invalid issues -> "unusable" issues
                 if (IVi >= FVi) {
-                    invalid++;
+                    unusable++;
                     continue;
-                } else if (IVi > HALF_LASTi) {
+                }
+                // skip "over half" issues
+                else if (IVi > HALF_LASTi) {
                     overHalf++;
                     continue;
                 } else {
-                    valid++;
+                    usable++;
                 }
 
+                // Obtain the list of the valid versions affected by the issue
                 List<Version> affectedVersions = vc.getAllValidVersions().subList(IVi, FVi);
 
+                // Obtain the list of the valid versions affected by the issue which are part of
+                // the half sublist
                 List<Version> datasetAffectedVersions = vc.getHalfVersion()
                         .stream()
                         .filter(affectedVersions::contains)
                         .filter(vc.getHalfVersion()::contains)
                         .toList();
+
+                // Obtain the list of the paths affected by the issue, without duplicates
+                List<Path> datasetAffectedPaths = new ArrayList<>(
+                        new LinkedHashSet<>(bc.getAllPathsTouchedByIssue(proportionedIssue)));
+
+                // Counter variable for printing purpose
+                var ref = new Object() {
+                    Integer i = 0;
+                };
+
+                /*
+                 Label the entries which path is contained in the affected paths AND
+                 which version is contained in the half affected version
+                 as buggy (is it hadn't been labelled already)
+                */
+                this.allLabelledEntries
+                        .stream()
+                        .filter(entry -> datasetAffectedVersions.contains(entry.getVersion()))
+                        .filter(entry -> datasetAffectedPaths.contains(entry.getPath()))
+                        .filter(Entry::isNotBuggy)
+                        .forEach(entry -> {
+                            newLabelledEntries.add(entry);
+                            entry.setBuggy(true);
+                            ref.i++;
+                        });
 
                 logger.logFinest(() -> System.out.println("*** proportionedIssue: " + proportionedIssue));
                 logger.logFinest(() -> {
@@ -78,34 +116,15 @@ public class BugginessController {
                         }
                 );
 
-                // Remove duplicates
-                List<Path> datasetAffectedPaths = new ArrayList<>(
-                        new LinkedHashSet<>(bc.getAllPathsTouchedByIssue(proportionedIssue)));
-
                 logger.logFinest(() ->
                         System.out.println("datasetAffectedPaths.size(): " + datasetAffectedPaths.size() + ", " + datasetAffectedPaths));
-
-                var ref = new Object() {
-                    Integer i = 0;
-                };
-
-                this.allLabelledEntries
-                        .stream()
-                        .filter(entry -> datasetAffectedVersions.contains(entry.getVersion()))
-                        .filter(entry -> datasetAffectedPaths.contains(entry.getPath()))
-                        .filter(Entry::isNotBuggy)
-                        .forEach(entry -> {
-                            newLabelledEntries.add(entry);
-                            entry.setBuggy(true);
-                            ref.i++;
-                        });
 
                 logger.logFinest(() ->
                         System.out.println("Number of entries labelled \"buggy\" (without duplicates): " + ref.i));
             }
 
-            int finalValid = valid;
-            int finalInvalid = invalid;
+            int finalValid = usable;
+            int finalInvalid = unusable;
             int finalOverHalf = overHalf;
 
             logger.logFine(() -> System.out.println("Total number of entries labelled  \"buggy\": " + newLabelledEntries.size()));
