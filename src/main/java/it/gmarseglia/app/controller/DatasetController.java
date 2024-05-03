8917e291986a757cac930a3705a119700d622de8
gmarseglia <g.marseglia.it@gmail.com>
2024-05-03 14:28:36 +0200

    close #55

--- src/main/java/it/gmarseglia/app/controller/DatasetController.java
+++ src/main/java/it/gmarseglia/app/controller/DatasetController.java
@@ -13,12 +13,14 @@ public class DatasetController {
 
     private final EntriesController ec;
     private final CsvEntryBoundary cb;
+    private final BugginessController bc;
     private final MyLogger logger = MyLogger.getInstance(this.getClass());
 
 
     private DatasetController(String projName) {
         this.ec = EntriesController.getInstance(projName);
         this.cb = new CsvEntryBoundary(projName);
+        this.bc = BugginessController.getInstance(projName);
     }
 
     public static DatasetController getInstance(String projName) {
@@ -37,7 +39,7 @@ public class DatasetController {
     public void populateDataset(boolean verbose) throws GitAPIException {
         MyLogger.setStaticVerbose(verbose);
 
-        List<Entry> allDatasetEntries = ec.getAllEntriesForHalfVersions();
+        List<Entry> allDatasetEntries = bc.getAllLabelledEntries();
 
         logger.log(() -> System.out.printf("Total entries size: %d\n", allDatasetEntries.size()));
 
