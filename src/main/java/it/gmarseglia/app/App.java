62e0f6bf079fcd0c3a785d6ff3d18c5c0a9377d4
gmarseglia <g.marseglia.it@gmail.com>
2024-05-04 15:03:43 +0200

    close #63

--- src/main/java/it/gmarseglia/app/App.java
+++ src/main/java/it/gmarseglia/app/App.java
@@ -1,23 +1,44 @@
 package it.gmarseglia.app;
 
+import it.gmarseglia.app.boundary.IssueJSONGetter;
 import it.gmarseglia.app.controller.*;
 import it.gmarseglia.app.entity.Issue;
 import it.gmarseglia.app.entity.Version;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.Arrays;
 import java.util.List;
+
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
 
 
 public class App {
     public static void main(String[] args) {
-        String projName = "BOOKKEEPER";
-        GitController.getInstance(projName).setTagsRegex("(release-)?%v(-incubating)?");
-
         MyLogger.setStaticVerbose(true);
         MyLogger.setStaticVerboseFine(true);
         MyLogger.setStaticVerboseFinest(false);
 
-        run(projName);
+        for (String projName : Arrays.asList("BOOKKEEPER", "OPENJPA")) {
+            MyLogger.getInstance(App.class).logPrefixless(() -> System.out.println("Producing dataset for " + projName));
+
+            GitController.getInstance(projName).setTagsRegex("(release-)?%v(-incubating)?");
+
+            Path performanceFile = Paths.get(".", "out", projName + "_performance.txt");
+            try {
+                Files.writeString(performanceFile, "Begin: " + Instant.now().toString().concat(System.lineSeparator()), CREATE, APPEND);
+                run(projName);
+                Files.writeString(performanceFile, "End: " + Instant.now().toString().concat(System.lineSeparator()), APPEND);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
     }
 
     private static void test(String projName) {
@@ -58,11 +79,10 @@ public class App {
     }
 
     private static void run(String projName) {
-
         DatasetController dc = DatasetController.getInstance(projName);
 
         try {
-            dc.populateDataset(true);
+            dc.populateDataset();
         } catch (GitAPIException e) {
             throw new RuntimeException(e);
         }
