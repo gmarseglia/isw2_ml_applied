c2ac95b5e24e91eb096e933ed3df222915f9cb92
gmarseglia <g.marseglia.it@gmail.com>
2024-05-05 19:48:15 +0200

    Modified `App.java`

--- src/main/java/it/gmarseglia/app/App.java
+++ src/main/java/it/gmarseglia/app/App.java
@@ -1,12 +1,11 @@
 package it.gmarseglia.app;
 
+import it.gmarseglia.app.boundary.ToFileBoundary;
 import it.gmarseglia.app.controller.*;
-import it.gmarseglia.app.entity.Version;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Instant;
 import java.util.List;
@@ -20,41 +19,24 @@ public class App {
         MyLogger.setStaticVerboseFine(true);
         MyLogger.setStaticVerboseFinest(false);
 
-        for (String projName : List.of("AVRO")) {
+        for (String projName : List.of("BOOKKEEPER", "OPENJPA", "SYNCOPE", "AVRO")) {
             MyLogger.getInstance(App.class).logNoPrefix("Project: " + projName);
 
             GitController.getInstance(projName).setTagsRegex("(release-)?(syncope-)?%v(-incubating)?");
 
-            MyFileUtils.createDirectory(Paths.get(".", "out", projName));
-            Path performanceFile = Paths.get(".", "out", projName, "performance.txt");
-            try {
-                Files.writeString(performanceFile, "Begin: \t" + Instant.now().toString().concat(System.lineSeparator()), CREATE, WRITE);
-                test(projName);
-                // run(projName);
-                Files.writeString(performanceFile, "End  : \t" + Instant.now().toString().concat(System.lineSeparator()), APPEND);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+            ToFileBoundary.writeStringProj(String.format("Begin: \t%s", Instant.now().toString()), projName, "performance.csv");
+            run(projName);
+            ToFileBoundary.writeStringProj(String.format("End  : \t%s", Instant.now().toString()), projName, "performance.csv");
+
         }
 
-    }
-
-    private static void test(String projName) {
-        try {
-//            MyLogger.getInstance(IssueController.class).setVerboseFine(false);
-            VersionsController.getInstance(projName).getHalfVersion();
-            ProportionController.getInstance(projName).getTotalProportionedIssues(Integer.MAX_VALUE);
-        } catch (GitAPIException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     private static void run(String projName) {
         DatasetController dc = DatasetController.getInstance(projName);
 
-        MyLogger.getInstance(Version.class).setVerboseFinest(true);
-
         try {
+            dc.setComputeMetrics(false);
             dc.populateDataset();
         } catch (GitAPIException e) {
             throw new RuntimeException(e);
