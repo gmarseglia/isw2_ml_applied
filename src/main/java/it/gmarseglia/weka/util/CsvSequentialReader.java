739f0a2d88f0450d4ce6c6cbd9fc6ab900abfd87
gmarseglia <g.marseglia.it@gmail.com>
2024-12-02 16:13:20 +0100

    #90: clean CsvSequentialReader.java

--- src/main/java/it/gmarseglia/weka/util/CsvSequentialReader.java
+++ src/main/java/it/gmarseglia/weka/util/CsvSequentialReader.java
@@ -1,20 +1,18 @@
 package it.gmarseglia.weka.util;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CsvSequentialReader {
 
+    private CsvSequentialReader(){}
+
     public static List<String> getFirstColumnEntries(Path csvFile) throws IOException {
         try (Stream<String> lines = Files.lines(csvFile).skip(1)) {
-            return lines.map(line -> line.split(",")[0])
-                    .collect(Collectors.toList());
+            return lines.map(line -> line.split(",")[0]).toList();
         }
     }
 }
