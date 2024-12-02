227e2389d9cde38d2c1ddead9857de2d07e0d331
gmarseglia <g.marseglia.it@gmail.com>
2024-12-02 12:23:05 +0100

    #90: clean MetricsController.java

--- src/main/java/it/gmarseglia/app/boundary/ProjectJSONGetter.java
+++ src/main/java/it/gmarseglia/app/boundary/ProjectJSONGetter.java
@@ -2,24 +2,18 @@ package it.gmarseglia.app.boundary;
 
 
 import it.gmarseglia.app.controller.JsonCacheUtils;
-import it.gmarseglia.app.controller.MyLogger;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
 
 public class ProjectJSONGetter {
 
-    private final MyLogger logger = MyLogger.getInstance(ProjectJSONGetter.class);
     private final String projName;
-    private final String urlBase = "https://issues.apache.org/jira/rest/api/2/project/";
+    private static final String URL_BASE = "https://issues.apache.org/jira/rest/api/2/project/";
 
     public ProjectJSONGetter(String projName) {
         this.projName = projName;
     }
 
     public String getUrl() {
-        return urlBase + this.projName;
+        return URL_BASE + this.projName;
     }
 
     public String getJSONFromResources() {
