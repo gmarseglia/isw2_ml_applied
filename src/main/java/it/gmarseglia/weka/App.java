61af35a41e18f76eb7ab7ae298565452536c3187
gmarseglia <g.marseglia.it@gmail.com>
2024-12-02 16:57:59 +0100

    #90: clean App.java (Weka)

--- src/main/java/it/gmarseglia/weka/App.java
+++ src/main/java/it/gmarseglia/weka/App.java
@@ -2,20 +2,21 @@ package it.gmarseglia.weka;
 
 import it.gmarseglia.app.boundary.ToFileBoundary;
 import it.gmarseglia.app.controller.MyLogger;
+import it.gmarseglia.app.exceptions.CustomRuntimeException;
 import it.gmarseglia.weka.controller.ExperimentsController;
 import it.gmarseglia.weka.controller.ExperimentsPlanner;
 import it.gmarseglia.weka.entity.ExperimentPlan;
 import it.gmarseglia.weka.util.Configs;
 import it.gmarseglia.weka.util.CsvToArffConverter;
 
+import java.io.IOException;
 import java.util.List;
 
 public class App {
     private static final MyLogger logger = MyLogger.getInstance(ExperimentsController.class);
 
     public static void main(String[] args) throws Exception {
-        List<String> projects = Configs.getProjects();
-        projects = List.of("BOOKKEEPER", "OPENJPA");
+        List<String> projects = List.of("BOOKKEEPER", "OPENJPA");
         CsvToArffConverter.convertProjects(projects);
 
         logger.setVerbose(true);
@@ -27,10 +28,14 @@ public class App {
 
     }
 
-    private static void run(String projName) throws Exception {
-        ExperimentPlan plan = ExperimentsPlanner.getInstance(projName).plan();
+    private static void run(String projName) {
+        ExperimentPlan plan = null;
+        try {
+            plan = ExperimentsPlanner.getInstance(projName).plan();
+        } catch (IOException e) {
+            throw new CustomRuntimeException(e);
+        }
         ExperimentsController.getInstance(projName).processExperimentPlan(plan);
-
         ToFileBoundary.writeList(plan.getAllResults(), Configs.getProjWekaOutDir("EXPERIMENTS"), projName + "-RESULTS.csv");
     }
 
